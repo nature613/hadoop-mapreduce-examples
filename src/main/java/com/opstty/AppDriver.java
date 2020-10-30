34dc7ad390d9be26142c7e694e192b642e7869e5
nature613 <chikonakamura613@gmail.com>
2020-10-30 18:16:42 +0100

    + Added DistinctDistricts job, mapper, reducer + Added "distincDistricts" command to AppDriver

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -1,5 +1,6 @@
 package com.opstty;
 
+import com.opstty.job.DistinctDistricts;
 import com.opstty.job.WordCount;
 import org.apache.hadoop.util.ProgramDriver;
 
@@ -11,7 +12,10 @@ public class AppDriver {
         try {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
-
+            
+            programDriver.addClass("distinctDistricts", DistinctDistricts.class,
+                    "A map/reduce program that returns the distinct districts with trees in a predefined CSV formatting.");
+            
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
             throwable.printStackTrace();
---
+++ src/main/java/com/opstty/reducer/TreesReducer.java
@@ -0,0 +1,20 @@
+package com.opstty.reducer;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+
+public class TreesReducer extends Reducer<Text, NullWritable,
+Text,
+NullWritable> { //IntWritable> {
+    public void reduce(Text key, Iterable<NullWritable> values, Context context)
+            throws IOException, InterruptedException {
+    	System.out.println(key);
+    	System.out.println(values);
+    	context.write(key, NullWritable.get());
+    }
+}
