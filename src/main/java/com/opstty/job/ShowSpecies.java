b9abc8bd25c4e292d8246016d4378f34e309b09e
nature613 <chikonakamura613@gmail.com>
2020-11-03 18:15:36 +0100

    added target (maven-generated jars)

---
+++ src/main/java/com/opstty/job/ShowSpecies.java
@@ -0,0 +1,39 @@
+package com.opstty.job;
+
+import com.opstty.mapper.TreesMapper;
+import com.opstty.mapper.TokenizerMapper;
+import com.opstty.reducer.TreesReducer;
+import com.opstty.reducer.IntSumReducer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class ShowSpecies {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length < 2) {
+            System.err.println("Usage: distinctDistricts <in> [<in>...] <out>");
+            System.exit(2);
+        }
+        Job job = Job.getInstance(conf, "distinctDistricts");
+        job.setJarByClass(ShowSpecies.class);
+        job.setMapperClass(TreesMapper.class);
+        job.setCombinerClass(TreesReducer.class);
+        job.setReducerClass(TreesReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(NullWritable.class);
+        for (int i = 0; i < otherArgs.length - 1; ++i) {
+            FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
+        }
+        FileOutputFormat.setOutputPath(job,
+                new Path(otherArgs[otherArgs.length - 1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
---
+++ src/main/java/com/opstty/mapper/SpeciesMapper.java
@@ -0,0 +1,20 @@
+package com.opstty.mapper;
+
+import org.apache.hadoop.io.LongWritable;
+        import org.apache.hadoop.io.NullWritable;
+        import org.apache.hadoop.io.Text;
+        import org.apache.hadoop.mapreduce.Mapper;
+        import java.io.IOException;
+
+public class SpeciesMapper extends Mapper<Object, Text, 
+		Text,
+		NullWritable> {
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+    	//context.write(NullWritable.get(), NullWritable.get());
+        if(!value.toString().contains("ARRONDISSEMENT")){
+            Text district = new Text(value.toString().split(";")[1]);
+            context.write(district, NullWritable.get());
+        }
+    }
+}
