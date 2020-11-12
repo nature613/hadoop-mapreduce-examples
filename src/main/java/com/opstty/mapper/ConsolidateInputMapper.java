6c4934ac6890bf6d4910f540ce8b76cd971d2e60
nature613 <chikonakamura613@gmail.com>
2020-11-12 23:07:47 +0100

    type fix ConsolidateInputMapper

--- src/main/java/com/opstty/mapper/ConsolidateInputMapper.java
+++ src/main/java/com/opstty/mapper/ConsolidateInputMapper.java
@@ -4,13 +4,14 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
-public class ConsolidateInputMapper extends Mapper<LongWritable, IntWritable, NullWritable, MapWritable> {
-	public void map(LongWritable key, IntWritable value, Context context) throws IOException, InterruptedException {
+public class ConsolidateInputMapper extends Mapper<LongWritable, Text, NullWritable, MapWritable> {
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 		MapWritable map = new MapWritable();
-		map.put(new IntWritable((int) key.get()), value);
+		map.put(new IntWritable((int) key.get()), new IntWritable(Integer.parseInt(value.toString())));
 		context.write(NullWritable.get(), map);
 	}
 }
