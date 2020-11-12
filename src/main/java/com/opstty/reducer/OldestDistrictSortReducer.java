f9d509226b6bd215bf783ccfeda278771d80029a
nature613 <chikonakamura613@gmail.com>
2020-11-12 13:49:42 +0100

    inverting key value in reducer (strange behavior)

--- src/main/java/com/opstty/reducer/OldestDistrictSortReducer.java
+++ src/main/java/com/opstty/reducer/OldestDistrictSortReducer.java
@@ -13,7 +13,7 @@ public class OldestDistrictSortReducer extends Reducer<IntWritable, IntWritable,
 			throws IOException, InterruptedException {
 			if (first) {
 				for (IntWritable district : values) {
-					context.write(district, key);
+					context.write(key, district);
 				}
 			} first = false;
 	}
